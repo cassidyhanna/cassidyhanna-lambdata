fdda14d1e12359184bc4f24849b0a26e0b508ae1
Cassidy Ellis <CassidyEllis@Cassidys-MacBook-Pro.local>
2020-03-25 20:25:18 -0700

    Demonstrate import nuances

--- Lambdata/my_mod.py
+++ Lambdata/my_mod.py
@@ -3,3 +3,12 @@
 
 def enlarge(n):
     return n * 100
+
+
+if __name__ == "__main__":
+
+    print("JUNK")
+    print("GLOBAL SCOPE")
+
+    y = float(input("PLEASE INPUT A NUMBER TO ENLARGE: "))
+    print(enlarge(y))
