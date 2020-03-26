4a1fbb2ac8ad24381963f5526370e714e777f064
Cassidy Ellis <CassidyEllis@Cassidys-MacBook-Pro.local>
2020-03-25 21:08:59 -0700

    Fix setup file

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 # setup.py file
+
 from setuptools import find_packages, setup
+
 with open("README.md", "r") as fh:
     long_description = fh.read()
+
 setup(
     name="cassidy-lambdata",
     version="1.0",
-    author="MJ Rossetti",
-    author_email="datacreativellc@gmail.com",
+    author="Cassidy Ellis",
+    author_email="cellis@pdx.edu",
     description="For example purposes",
     long_description=long_description,
     # required if using a md file for long desc
