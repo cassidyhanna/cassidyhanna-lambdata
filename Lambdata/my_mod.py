5e5d706907e10cc3894ecde29e3bb321f55eea40
Cassidy Ellis <CassidyEllis@Cassidys-MacBook-Pro.local>
2020-04-01 14:41:58 -0700

    Add docstring

--- Lambdata/my_mod.py
+++ Lambdata/my_mod.py
@@ -2,6 +2,13 @@
 
 
 def enlarge(n):
+    """
+    Multiplies a number by 100 
+
+    Param: n (numeric) the number to enlarge
+
+    Return the enlarged number (numeric)
+    """
     return n * 100
 
 
