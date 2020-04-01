29a621351a451bdd1d482df0f679e123e1f2487c
Cassidy Ellis <CassidyEllis@Cassidys-MacBook-Pro.local>
2020-04-01 16:35:04 -0700

    Start with OPP Approach

--- Lambdata/teams.py
+++ Lambdata/teams.py
@@ -2,6 +2,12 @@
 
 # example teams.py (functional approach)
 
+class Team():
+    def __init__(self, name, city):
+        self.name = name
+        self.city = city
+
+
 def advertise(my_team):
     print(f"HEY COME TO {my_team['city'].upper()} TO SEE OUR GAMES!!!")
 
@@ -11,7 +17,7 @@ def full_name(my_team):
 
 
 if __name__ == "__main__":
-    teams = [
+    '''teams = [
         {"city": "New York", "name": "Yankees"},
         {"city": "New York", "name": "Mets"},
         {"city": "Boston", "name": "Red Sox"},
@@ -23,3 +29,20 @@ if __name__ == "__main__":
         print("-------")
         print(full_name(team))
         advertise(team)
+    '''
+    # initialize / create an instance of the object
+    team = Team(city="Washington", name="Wizards")
+    print(team)
+    print(type(team))
+    # invoking attributes
+    print(team.name)
+    # invoking attributes
+    print(team.city)
+
+    # initialize / create an instance of the object
+    team2 = Team("Giants", "New York")
+    print(type(team2))
+    # invoking attributes
+    print(team2.name)
+    # invoking attributes
+    print(team2.city)
