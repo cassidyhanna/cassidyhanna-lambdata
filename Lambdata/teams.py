f4f95b2b9bcf98f6e72aadc1f08f0b11e2740706
Cassidy Ellis <CassidyEllis@Cassidys-MacBook-Pro.local>
2020-04-01 19:31:55 -0700

    OOP and default params

--- Lambdata/teams.py
+++ Lambdata/teams.py
@@ -1,23 +1,23 @@
 
 
-# example teams.py (functional approach)
+# example teams.py ( OOP approach)
 
 class Team():
-    def __init__(self, name, city):
+    def __init__(self, name, city="CITY NAME", players=["Player 1"]):
         self.name = name
         self.city = city
+        self.players = players
 
+    def advertise(self):
+        print(f"HEY COME TO {self.city.upper()} TO SEE OUR GAMES!!!")
 
-def advertise(my_team):
-    print(f"HEY COME TO {my_team['city'].upper()} TO SEE OUR GAMES!!!")
-
-
-def full_name(my_team):
-    return f"{my_team['city']} {my_team['name']}"
+    @property
+    def full_name(self):
+        return f"{self.city} {self.name}"
 
 
 if __name__ == "__main__":
-    '''teams = [
+    teams = [
         {"city": "New York", "name": "Yankees"},
         {"city": "New York", "name": "Mets"},
         {"city": "Boston", "name": "Red Sox"},
@@ -25,24 +25,10 @@ if __name__ == "__main__":
         {"city": "Washington", "name": "Nationals"}
     ]
 
-    for team in teams:
+    for team_attributes in teams:
+        team = Team(name=team_attributes["name"], city=team_attributes["city"])
         print("-------")
-        print(full_name(team))
-        advertise(team)
-    '''
-    # initialize / create an instance of the object
-    team = Team(city="Washington", name="Wizards")
-    print(team)
-    print(type(team))
-    # invoking attributes
-    print(team.name)
-    # invoking attributes
-    print(team.city)
-
-    # initialize / create an instance of the object
-    team2 = Team("Giants", "New York")
-    print(type(team2))
-    # invoking attributes
-    print(team2.name)
-    # invoking attributes
-    print(team2.city)
+        print(team.city)
+        print(team.full_name)
+        print(team.players)
+        team.advertise()
